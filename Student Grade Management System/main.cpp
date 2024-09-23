e8c0a7b407a494271c9c17c3d113758f5eed7468
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-09-23 10:00:22 -0700

    Initial Commit

---
+++ Student Grade Management System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Student Grade Management System
+//
+//  Created by Isaac Adeleke on 9/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
